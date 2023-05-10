23bc5888eb82903f598cd735d22feb2231a06dce
watashi <watashi@watashi>
2023-05-10 12:30:05 +0900

    Update lesson3.py

--- data_science_streamlit/lesson3.py
+++ data_science_streamlit/lesson3.py
@@ -20,6 +20,8 @@ Original file is located at
 
 # Commented out IPython magic to ensure Python compatibility.
 # %%writefile {dir_name}/main_app.py
+
+#これらをインストールすること！
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
